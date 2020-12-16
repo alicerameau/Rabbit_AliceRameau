350b7dc39703d627ecfe3359786f4b89b27708ce
Alice Rameau <alice.rameau.auditeur@lecnam.net>
2020-12-16 14:46:39 +0100

    Collectibles and score
    Mise en place d'un système de collectibles influant sur le score du joueur

---
+++ Assets/Scripts/CollectCoins.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectCoins : MonoBehaviour
+{
+    public int score = 0;
+
+    void OnTriggerEnter(Collider col)
+    {
+       
+        if (col.tag == "Coin")
+        {
+            Destroy(col.gameObject);
+            score += 1;
+        }
+    }
+}
